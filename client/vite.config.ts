8c8fd8cd6dfbdde09a19ed677c80c92f053da470
Josh Gillespie <jcgillespie@users.noreply.github.com>
2026-03-25 11:53:28 -0500

    adding vite server config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1',
+    port:5173,
+  },
   test: {
     environment: 'jsdom',
     setupFiles: './src/test/setupTests.ts',
